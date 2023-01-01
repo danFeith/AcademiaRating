76f4dc04ba9bcc8b3ab8734a5b8e20cf1fc92561
DanFeith <d.feithlicher@gmail.com>
2023-01-01 16:28:29 +0200

    change auth config to original expiration values

--- Server/src/config/auth.config.js
+++ Server/src/config/auth.config.js
@@ -1,11 +1,11 @@
 const authConfig = {
     secret: "academiarating-secret-key",
-    //jwtExpiration: 3600,           // 1 hour
-    //jwtRefreshExpiration: 86400,   // 24 hours
+    jwtExpiration: 3600,           // 1 hour
+    jwtRefreshExpiration: 86400,   // 24 hours
 
     /* for test */
-    jwtExpiration: 60,          // 1 minute
-    jwtRefreshExpiration: 120,  // 2 minutes
+    //jwtExpiration: 60,          // 1 minute
+    //jwtRefreshExpiration: 120,  // 2 minutes
 };
 
 export default authConfig;
